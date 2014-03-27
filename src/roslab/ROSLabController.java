beed070f95a026806b3f002cf4b9cf56eada90e1
Peter Gebhard <pgeb@seas.upenn.edu>
2014-03-27 17:23:11 -0400

    Added TreeView.

--- src/roslab/ROSLabController.java
+++ src/roslab/ROSLabController.java
@@ -1,5 +1,21 @@
 package roslab;
 
-public class ROSLabController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TreeView;
+
+public class ROSLabController implements Initializable {
+	
+	@FXML
+	TreeView<String> tree;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
