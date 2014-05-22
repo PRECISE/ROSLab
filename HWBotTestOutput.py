09f8e327adbf3ec7861922be9b1e9592138dda70
Peter Gebhard <pgeb@seas.upenn.edu>
2014-05-22 13:54:27 -0400

    Updated HWBot template slightly, added HWBotTestOutput.py

--- HWBotTestOutput.py
+++ HWBotTestOutput.py
@@ -8,24 +8,35 @@ class Bot(Component):
   def defComponents(self):
     #self.addComponent("brain", Brains, None)
     #self.addComponent("half", HalfAnt, None)
-    $add_components$
+    self.addComponent("brain", Brains, None)
+    self.addComponent("half1", HalfAnt, None)
+    self.addComponent("half2", HalfAnt, None)
+
 
   def defSubParameters(self):
     #self.setSubParameter("half", "width", self.getParameter("brain").getParameter("width") + self.getParameter("servo").getParameter("motorheight") * 2)
-    $set_sub_parameters$
+    self.setSubParameter("brain", "length", 48)
+    self.setSubParameter("brain", "height", 25)
+    self.setSubParameter("brain", "hardware", proMini)
+    self.setSubParameter("half1", "width", self.getParameter('brain').getParameter('width'))
+    self.setSubParameter("half2", "width", self.getParameter('brain').getParameter('width'))
+
 
   def assemble(self):
     ### Assemble the object
 
     # Insert the main beam (first append and then attach things to that)
     #self.append("brain", "core")
-    $append_components$
+    self.append("brain", "core")
+
 
     #self.attach(("brain", "core", "botright"),
     #            ("half", "front", "topright"), Fold(-180) )
     #self.attach(("brain", "core", "topleft"),
     #            ("half", "back", "topright"), Fold(-180) )
-    $attach_components$
+    self.attach(("brain", "core", "botright"), ("half1", "half1", "topright"), Fold(-180))
+    self.attach(("brain", "core", "topleft"), ("half2", "half2", "topright"), Fold(-180))
+
 
     #self.addTabs((Tab(), "tabfront", 9),
     #             ("half", "front", "botedge.2"),
@@ -33,7 +44,9 @@ class Bot(Component):
     #self.addTabs((Tab(), "tabback", 9),
     #             ("half", "back", "botedge.2"),
     #             ("half", "front", "topedge.3"))
-    $add_tabs$
+    self.addTabs((Tab(), "tabhalf1", 9), ("half1", "half1", "botedge.2"), ("half1", "half1", "topedge.3")
+    self.addTabs((Tab(), "tabhalf2", 9), ("half2", "half2", "botedge.2"), ("half2", "half2", "topedge.3")
+
 
 if __name__ == "__main__":
 
@@ -49,7 +62,10 @@ if __name__ == "__main__":
   #f.setParameter("brain", proMini)
   #f.setParameter("length", 48)
   #f.setParameter("height", 25)
-  $set_parameters$
+  f.setParameter("brain", proMini)
+  f.setParameter("length", 48)
+  f.setParameter("height", 25)
+
 
   print "Compiling robot designs...",
   sys.stdout.flush()
