0e56188fcd80abeee5dc5b3d477e0a44f3958027
Peter Gebhard <pgeb@seas.upenn.edu>
2014-05-23 07:41:07 -0400

    Added selection rectangle to ROSLabController.

--- src/roslab/ROSLabController.java
+++ src/roslab/ROSLabController.java
@@ -1,23 +1,26 @@
 package roslab;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
-import roslab.model.general.Configuration;
-import roslab.model.general.Library;
-import roslab.model.general.Node;
-import roslab.model.ui.UINode;
-import javafx.beans.value.ChangeListener;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import roslab.model.electronics.Circuit;
+import roslab.model.general.Configuration;
+import roslab.model.general.Library;
+import roslab.model.general.Node;
+import roslab.model.software.ROSNode;
+import roslab.model.ui.UINode;
 
 public class ROSLabController implements Initializable {
 	
@@ -35,18 +38,67 @@ public class ROSLabController implements Initializable {
 	
 	Library library;
 	Configuration config;
+	Rectangle selectionRectangle;
+	double selectionX;
+	double selectionY;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		assert tree != null : "fx:id\"tree\" was not injected";
-		System.out.println(this.getClass().getSimpleName() + ".initialize");
+		//System.out.println(this.getClass().getSimpleName() + ".initialize");
+		//TODO: get highlighting and selection of Nodes based on selection rectangle
+		//enableSelectionRectangle(swPane);
+		//enableSelectionRectangle(hwPane);
+		//enableSelectionRectangle(eePane);
 		loadTree();
-		swPane.getChildren().add(new UINode(null, null, 0, 0));
-		hwPane.getChildren().add(new UINode(null, null, 0, 0));
-		eePane.getChildren().add(new UINode(null, null, 0, 0));
+		Random r = new Random();
+		for(int i = 0; i < 1000; i++) {
+			swPane.getChildren().add(UINode.buildUINode(new ROSNode("test", null, null, null), null, r.nextInt(2000), r.nextInt(2000)));
+		}
+		//swPane.getChildren().add(UINode.buildUINode(new Circuit("test", null, null, null), null, r.nextInt(2000), r.nextInt(2000)));
 		tree.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		
+		
 	}
 	
+	private void enableSelectionRectangle(final Pane p) {
+		p.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				selectionX = mouseEvent.getX();
+				selectionY = mouseEvent.getY();
+				selectionRectangle = new Rectangle(selectionX, selectionY, 0, 0);
+				selectionRectangle.getStyleClass().add("SelectionRectangle");
+				p.getChildren().add(selectionRectangle);
+			}
+		});
+		p.setOnMouseReleased(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				p.getChildren().remove(selectionRectangle);
+			}
+		});
+		p.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				double x = mouseEvent.getX();
+				double y = mouseEvent.getY();
+				if (x < selectionX) {
+					selectionRectangle.setX(x);
+					selectionRectangle.setWidth(selectionX - x);
+				} else {
+					selectionRectangle.setWidth(x - selectionRectangle.getX());
+				}
+				if (y < selectionY) {
+					selectionRectangle.setY(y);
+					selectionRectangle.setHeight(selectionY - y);
+				} else {
+					selectionRectangle.setHeight(y - selectionRectangle.getY());
+				}
+			}
+		});
+	}
+
 	private void openLibrary() {
 		
 	}
