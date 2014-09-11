4a976578a116c990aee26a32b3fec84df8bd2b70
Peter Gebhard <pgeb@seas.upenn.edu>
2014-09-11 17:52:12 -0400

    Changed Node class to not use JavaFX properties (for now).

--- src/roslab/model/general/Node.java
+++ src/roslab/model/general/Node.java
@@ -1,123 +1,130 @@
 /**
- * 
+ *
  */
 package roslab.model.general;
 
 import java.util.Map;
-import java.util.Set;
 
-import javafx.beans.property.MapProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleMapProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableMap;
 import roslab.model.ui.UINode;
 
 /**
  * @author shaz
- *
  */
 public class Node {
-	
-	protected StringProperty name;
-	protected MapProperty<String, ? extends Feature> features;
-	protected MapProperty<String, String> annotations;
-	protected ObjectProperty<UINode> uiNode;
-	
-	/**
-	 * @param name
-	 * @param feature
-	 * @param annotations
-	 */
-	public Node(String name, Map<String, ? extends Feature> features,
-			Map<String, String> annotations) {
-		this.name = new SimpleStringProperty(name);
-		this.features = new SimpleMapProperty(FXCollections.observableMap(features));
-		this.annotations = new SimpleMapProperty(FXCollections.observableMap(annotations));
-	}
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+    protected String name;
+    protected Map<String, ? extends Feature> features;
+    protected Map<String, String> annotations;
+    protected UINode uiNode;
 
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * @param name
+     * @param feature
+     * @param annotations
+     */
+    public Node(String name, Map<String, ? extends Feature> features, Map<String, String> annotations) {
+        // TODO: Use StringProperty and other <x>Property types for better
+        // integration in GUI?
+        this.name = name;
+        this.features = features;
+        this.annotations = annotations;
+    }
 
-	/**
-	 * @return the features
-	 */
-	public Map<String, ? extends Feature> getFeatures() {
-		return features;
-	}
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * @param features the features to set
-	 */
-	public void setFeatures(Map<String, ? extends Feature> features) {
-		this.features = features;
-	}
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	/**
-	 * @return the annotations
-	 */
-	public Map<String, String> getAnnotations() {
-		return annotations;
-	}
+    /**
+     * @return the features
+     */
+    public Map<String, ? extends Feature> getFeatures() {
+        return features;
+    }
 
-	/**
-	 * @param annotations the annotations to set
-	 */
-	public void setAnnotations(Map<String, String> annotations) {
-		this.annotations = annotations;
-	}
+    /**
+     * @param features
+     *            the features to set
+     */
+    public void setFeatures(Map<String, ? extends Feature> features) {
+        this.features = features;
+    }
 
-	/**
-	 * @return the annotations
-	 */
-	public String getAnnotation(String s) {
-		return annotations.get(s);
-	}
+    /**
+     * @param name
+     *            the name of the feature to get
+     * @return the feature
+     */
+    public Feature getFeature(String name) {
+        return this.features.get(name);
+    }
 
-	/**
-	 * @param annotations the annotations to set
-	 */
-	public void setAnnotation(String key, String value) {
-		this.annotations.put(key, value);
-	}
+    /**
+     * @return the annotations
+     */
+    public Map<String, String> getAnnotations() {
+        return annotations;
+    }
 
-	/**
-	 * @return the uiNode
-	 */
-	public UINode getUiNode() {
-		return uiNode;
-	}
+    /**
+     * @param annotations
+     *            the annotations to set
+     */
+    public void setAnnotations(Map<String, String> annotations) {
+        this.annotations = annotations;
+    }
 
-	/**
-	 * @param uiNode the uiNode to set
-	 */
-	public void setUiNode(UINode uiNode) {
-		this.uiNode = uiNode;
-	}
+    /**
+     * @return the annotation
+     */
+    public String getAnnotation(String s) {
+        return annotations.get(s);
+    }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return name;
-	}
-	
-	public static void addLink(Node src, Node dest) {
-		
-	}
+    /**
+     * @param annotations
+     *            the annotations to set
+     */
+    public void addAnnotation(String key, String value) {
+        this.annotations.put(key, value);
+    }
+
+    /**
+     * @return the uiNode
+     */
+    public UINode getUiNode() {
+        return uiNode;
+    }
+
+    /**
+     * @param uiNode
+     *            the uiNode to set
+     */
+    public void setUiNode(UINode uiNode) {
+        this.uiNode = uiNode;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static void addLink(Node src, Node dest) {
+
+    }
 
 }
