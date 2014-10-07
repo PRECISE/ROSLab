d1934696579c7027e0c3997112b1a661ffbdd8f3
Peter Gebhard <pgeb@seas.upenn.edu>
2014-10-07 12:47:59 -0400

    Formatting cleanup.

--- src/roslab/model/hardware/Joint.java
+++ src/roslab/model/hardware/Joint.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package roslab.model.hardware;
 
@@ -10,48 +10,51 @@ import roslab.model.general.Feature;
 
 /**
  * @author Peter Gebhard
- *
  */
 public class Joint extends Feature implements Endpoint {
-	
-	boolean fanIn;
-	boolean fanOut;
 
-	/**
-	 * @param name
-	 * @param parent
-	 * @param annotations
-	 * @param fanIn
-	 * @param fanOut
-	 */
-	public Joint(String name, HWBlock parent, Map<String, String> annotations, boolean fanIn, boolean fanOut) {
-		super(name, parent, annotations);
-		this.fanIn = fanIn;
-		this.fanOut = fanOut;
-	}
+    boolean fanIn;
+    boolean fanOut;
 
-	/* (non-Javadoc)
-	 * @see roslab.model.general.Endpoint#isFanIn()
-	 */
-	@Override
-	public boolean isFanIn() {
-		return fanIn;
-	}
+    /**
+     * @param name
+     * @param parent
+     * @param annotations
+     * @param fanIn
+     * @param fanOut
+     */
+    public Joint(String name, HWBlock parent, Map<String, String> annotations, boolean fanIn, boolean fanOut) {
+        super(name, parent, annotations);
+        this.fanIn = fanIn;
+        this.fanOut = fanOut;
+    }
 
-	/* (non-Javadoc)
-	 * @see roslab.model.general.Endpoint#isFanOut()
-	 */
-	@Override
-	public boolean isFanOut() {
-		return fanOut;
-	}
+    /*
+     * (non-Javadoc)
+     * @see roslab.model.general.Endpoint#isFanIn()
+     */
+    @Override
+    public boolean isFanIn() {
+        return fanIn;
+    }
 
-	/* (non-Javadoc)
-	 * @see roslab.model.general.Endpoint#canConnect(roslab.model.general.Endpoint)
-	 */
-	@Override
-	public boolean canConnect(Endpoint e) {
-		return true;
-	}
+    /*
+     * (non-Javadoc)
+     * @see roslab.model.general.Endpoint#isFanOut()
+     */
+    @Override
+    public boolean isFanOut() {
+        return fanOut;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see
+     * roslab.model.general.Endpoint#canConnect(roslab.model.general.Endpoint)
+     */
+    @Override
+    public boolean canConnect(Endpoint e) {
+        return true;
+    }
 
 }
