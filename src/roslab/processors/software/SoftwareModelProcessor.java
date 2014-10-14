be264dfa4f3f943e1802e0a9480fde121d55e7ec
Peter Gebhard <pgeb@seas.upenn.edu>
2014-10-14 16:00:18 -0400

    Organized processor classes.

---
+++ src/roslab/processors/software/SoftwareModelProcessor.java
@@ -0,0 +1,31 @@
+/**
+ *
+ */
+package roslab.processors.software;
+
+import roslab.model.general.Configuration;
+import roslab.processors.ModelProcessor;
+
+/**
+ * @author shaz
+ */
+public class SoftwareModelProcessor extends ModelProcessor {
+
+    /**
+     * @param config
+     */
+    public SoftwareModelProcessor(Configuration config) {
+        super(config);
+        // TODO Auto-generated constructor stub
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see roslab.processors.ModelProcessor#output()
+     */
+    @Override
+    public String output() {
+        return st.render();
+    }
+
+}
