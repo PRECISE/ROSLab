863351a05e9a1adc880863b3754b5177cb221f30
Peter Gebhard <pgeb@seas.upenn.edu>
2014-10-14 15:59:50 -0400

    Changed field visibility to protected.

--- src/roslab/processors/ModelProcessor.java
+++ src/roslab/processors/ModelProcessor.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package roslab.processors;
 
@@ -11,23 +11,22 @@ import roslab.model.general.Configuration;
 
 /**
  * @author Peter Gebhard
- *
  */
 public abstract class ModelProcessor {
-	
-	Configuration config;
-	STGroup stg = new STRawGroupDir("templates");
-	ST st;
 
-	/**
-	 * 
-	 */
-	public ModelProcessor(Configuration config) {
-		this.config = config;
-		stg.delimiterStartChar = '$';
-		stg.delimiterStopChar = '$';
-	}
-	
-	public abstract String output();
+    protected Configuration config;
+    protected STGroup stg = new STRawGroupDir("templates");
+    protected ST st;
+
+    /**
+     *
+     */
+    public ModelProcessor(Configuration config) {
+        this.config = config;
+        stg.delimiterStartChar = '$';
+        stg.delimiterStopChar = '$';
+    }
+
+    public abstract String output();
 
 }
