836acca9d0869793d17efc7dcdcabbddc42f16af
Peter Gebhard <pgeb@seas.upenn.edu>
2014-11-24 18:02:15 -0500

    Commented out blocks to be updated later for version 2 data model.

--- src/roslab/processors/mechanics/pythonlib/PythonLibraryHelper.java
+++ src/roslab/processors/mechanics/pythonlib/PythonLibraryHelper.java
@@ -5,7 +5,6 @@ package roslab.processors.mechanics.pythonlib;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.DirectoryStream;
@@ -14,18 +13,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
-import java.util.StringTokenizer;
 
 import org.python.core.PyObject;
 import org.python.util.PythonInterpreter;
 import org.yaml.snakeyaml.Yaml;
 
-import com.sun.xml.internal.ws.wsdl.writer.document.PortType;
+import roslab.model.mechanics.HWBlock;
+import roslab.model.mechanics.Joint;
 
 /**
  * @author shaz
@@ -77,7 +75,6 @@ public class PythonLibraryHelper {
         Yaml yaml = new Yaml();
 
         Map<String, Object> yam = (Map<String, Object>) yaml.load(input);
-        // System.out.println(yam);
 
         List<Connection> connections = new ArrayList<Connection>();
         for (List<Object> c : (List<List<Object>>) yam.get("connections")) {
@@ -122,140 +119,113 @@ public class PythonLibraryHelper {
         PyObject subcomponents = interp.eval(componentName + "().subcomponents");
         System.out.println("subcomponents: " + subcomponents);
 
-        // List<Connection> connections = new ArrayList<Connection>();
-        // for(List<Object> c : (List<List<Object>>) yam.get("connections")) {
-        // Connection conn = new Connection((List<String>) c.get(0),
-        // (List<String>) c.get(1), ConnectionType.valueOf((String) c.get(2)),
-        // (Map<String, String>) c.get(3));
-        // connections.add(conn);
-        // }
-        // Map<String, Map<String,String>> interfaces = new HashMap<String,
-        // Map<String,String>>();
-        // for(Entry<String, Map<String,String>> i :
-        // ((Map<String,Map<String,String>>) yam.get("interfaces")).entrySet())
-        // {
-        // interfaces.put(i.getKey(), i.getValue());
-        // }
-        // Map<String, String> parameters = (Map<String, String>)
-        // yam.get("parameters");
-        // Map<String, ComponentDetails> subcomponents = new HashMap<String,
-        // ComponentDetails>();
-        // for(Entry<String, Map<String,Object>> i :
-        // ((Map<String,Map<String,Object>>)
-        // yam.get("subcomponents")).entrySet()) {
-        // ComponentDetails details = new ComponentDetails((String)
-        // i.getValue().get("object"), (Map<String, List<String>>)
-        // i.getValue().get("parameters"));
-        // subcomponents.put(i.getKey(), details);
-        // }
-        // return new Component(connections, interfaces, parameters,
-        // subcomponents);
         return null;
     }
 
-    public ModuleSpec getDefaultModuleSpec(String compName) {
-        LinkedList<PortName> sendPortNames = new LinkedList<PortName>();
-        LinkedList<PortType> sendPortTypes = new LinkedList<PortType>();
-        sendPortNames.add(new PortName("topright"));
-        sendPortNames.add(new PortName("bottomright"));
-        sendPortNames.add(new PortName("topleft"));
-        sendPortNames.add(new PortName("bottomleft"));
-        PortType type = new PortType(PortTypes.JOINT_TYPE_NAME);
-        type.rosType = PortTypes.getJointType().rosType;
-        sendPortTypes.add(type);
-        sendPortTypes.add(type);
-        sendPortTypes.add(type);
-        sendPortTypes.add(type);
-        ComponentSignature defCompSig = new ComponentSignature(compName, sendPortNames, new LinkedList<PortName>(), sendPortTypes,
-                new LinkedList<PortType>(), new LinkedList<TaskDescriptor>());
-        ModuleSpec def = new ModuleSpec(compName, "ROS Service", defCompSig, null);
-        def.subtype = "joint";
-        return def;
+    public HWBlock getDefaultHWBlock(String blockName) {
+        HWBlock block = new HWBlock(blockName, null, null, null, null);
+        block.addJoint(new Joint("topright", block, null, false, false));
+        block.addJoint(new Joint("bottomright", block, null, false, false));
+        block.addJoint(new Joint("topleft", block, null, false, false));
+        block.addJoint(new Joint("bottomleft", block, null, false, false));
+        return block;
     }
 
-    public ModuleSpec getModuleSpecForComponent(String compName, Component comp) {
-        LinkedList<PortName> sendPortNames = new LinkedList<PortName>();
-        LinkedList<PortType> sendPortTypes = new LinkedList<PortType>();
-        PortType type = new PortType(PortTypes.JOINT_TYPE_NAME);
-        type.rosType = PortTypes.getJointType().rosType;
+    // public ModuleSpec getModuleSpecForComponent(String compName, Component
+    // comp) {
+    // LinkedList<PortName> sendPortNames = new LinkedList<PortName>();
+    // LinkedList<PortType> sendPortTypes = new LinkedList<PortType>();
+    // PortType type = new PortType(PortTypes.JOINT_TYPE_NAME);
+    // type.rosType = PortTypes.getJointType().rosType;
+    //
+    // for (Entry<String, Map<String, String>> en :
+    // comp.getInterfaces().entrySet()) {
+    // sendPortNames.add(new PortName(en.getKey()));
+    // sendPortTypes.add(type);
+    // }
+    // ComponentSignature sig = new ComponentSignature(compName, sendPortNames,
+    // new LinkedList<PortName>(), sendPortTypes,
+    // new LinkedList<PortType>(), new LinkedList<TaskDescriptor>());
+    // ModuleSpec def = new ModuleSpec(compName, "ROS Service", sig,
+    // comp.clone());
+    // def.subtype = "joint";
+    //
+    // return def;
+    // }
 
-        for (Entry<String, Map<String, String>> en : comp.getInterfaces().entrySet()) {
-            sendPortNames.add(new PortName(en.getKey()));
-            sendPortTypes.add(type);
-        }
-        ComponentSignature sig = new ComponentSignature(compName, sendPortNames, new LinkedList<PortName>(), sendPortTypes,
-                new LinkedList<PortType>(), new LinkedList<TaskDescriptor>());
-        ModuleSpec def = new ModuleSpec(compName, "ROS Service", sig, comp.clone());
-        def.subtype = "joint";
-
-        return def;
-    }
-
-    public void saveYAMLForConfiguration(ConfigurationSpec config) {
-        FileWriter output = null;
-        try {
-            output = new FileWriter(FileSystems.getDefault().getPath(library.toString(), config.configurationName + ".yaml").toFile());
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Yaml yaml = new Yaml();
-
-        List<Connection> connections = new ArrayList<Connection>();
-        for (Channel c : config.channels.values()) {
-            connections.add(c.getConnection());
-        }
-
-        Map<String, Map<String, String>> interfaces = new HashMap<String, Map<String, String>>();
-        for (Interface i : config.interfaces) {
-            interfaces.put(i.getName(), i.getParameters());
-        }
-
-        Map<String, String> parameters = new HashMap<String, String>();
-        for (int i = 0; config.params[i][0] != null; i++) {
-            parameters.put(config.params[i][0], config.params[i][1]);
-        }
-
-        Map<String, Map<String, Object>> subcomponents = new HashMap<String, Map<String, Object>>();
-        for (Entry<String, Pair<ModuleSpec, ComponentLocation>> e : config.configurationComponents.entrySet()) {
-            Map<String, List<String>> modParams = new HashMap<String, List<String>>();
-            Map<String, String> params = e.getValue().fst.pyComp.getParameters();
-            for (Entry<String, String> en : params.entrySet()) {
-                List<String> plist = new ArrayList<String>();
-                StringTokenizer st = new StringTokenizer(en.getValue() != null ? en.getValue() : "", ",");
-                while (st.hasMoreTokens()) {
-                    plist.add(st.nextToken().trim());
-                }
-                modParams.put(en.getKey(), plist);
-            }
-            Map<String, Object> details = new HashMap<String, Object>();
-            details.put("object", e.getValue().fst.type);
-            details.put("parameters", modParams);
-            subcomponents.put(e.getKey(), details);
-        }
-
-        // Dump YAML to file
-        // yaml.dump(new Component(connections, interfaces, parameters,
-        // subcomponents), output);
-        Map<String, Object> comp = new HashMap<String, Object>();
-        List<List<Object>> conns = new ArrayList<List<Object>>();
-        for (Connection c : connections) {
-            conns.add(c.getGenericList());
-        }
-        comp.put("connections", conns);
-        comp.put("interfaces", interfaces);
-        comp.put("parameters", parameters);
-        comp.put("subcomponents", subcomponents);
-        yaml.dump(comp, output);
-
-        try {
-            output.close();
-        }
-        catch (IOException e1) {
-            e1.printStackTrace();
-        }
-    }
+    // public void saveYAMLForConfiguration(Configuration config) {
+    // FileWriter output = null;
+    // try {
+    // output = new
+    // FileWriter(FileSystems.getDefault().getPath(library.toString(),
+    // config.getName() + ".yaml").toFile());
+    // }
+    // catch (IOException e) {
+    // e.printStackTrace();
+    // }
+    //
+    // Yaml yaml = new Yaml();
+    //
+    // List<Connection> connections = new ArrayList<Connection>();
+    // for (Link l : config.getLinks()) {
+    // connections.add(new Connection(l.getSrc().getName(),
+    // l.getDest().getName(), config.));
+    // }
+    //
+    // Map<String, Map<String, String>> interfaces = new HashMap<String,
+    // Map<String, String>>();
+    // for (Interface i : config.interfaces) {
+    // interfaces.put(i.getName(), i.getParameters());
+    // }
+    //
+    // Map<String, String> parameters = new HashMap<String, String>();
+    // for (int i = 0; config.params[i][0] != null; i++) {
+    // parameters.put(config.params[i][0], config.params[i][1]);
+    // }
+    //
+    // Map<String, Map<String, Object>> subcomponents = new HashMap<String,
+    // Map<String, Object>>();
+    // for (Entry<String, Pair<ModuleSpec, ComponentLocation>> e :
+    // config.configurationComponents.entrySet()) {
+    // Map<String, List<String>> modParams = new HashMap<String,
+    // List<String>>();
+    // Map<String, String> params = e.getValue().fst.pyComp.getParameters();
+    // for (Entry<String, String> en : params.entrySet()) {
+    // List<String> plist = new ArrayList<String>();
+    // StringTokenizer st = new StringTokenizer(en.getValue() != null ?
+    // en.getValue() : "", ",");
+    // while (st.hasMoreTokens()) {
+    // plist.add(st.nextToken().trim());
+    // }
+    // modParams.put(en.getKey(), plist);
+    // }
+    // Map<String, Object> details = new HashMap<String, Object>();
+    // details.put("object", e.getValue().fst.type);
+    // details.put("parameters", modParams);
+    // subcomponents.put(e.getKey(), details);
+    // }
+    //
+    // // Dump YAML to file
+    // // yaml.dump(new Component(connections, interfaces, parameters,
+    // // subcomponents), output);
+    // Map<String, Object> comp = new HashMap<String, Object>();
+    // List<List<Object>> conns = new ArrayList<List<Object>>();
+    // for (Connection c : connections) {
+    // conns.add(c.getGenericList());
+    // }
+    // comp.put("connections", conns);
+    // comp.put("interfaces", interfaces);
+    // comp.put("parameters", parameters);
+    // comp.put("subcomponents", subcomponents);
+    // yaml.dump(comp, output);
+    //
+    // try {
+    // output.close();
+    // }
+    // catch (IOException e1) {
+    // e1.printStackTrace();
+    // }
+    // }
 
     public static void main(String[] args) {
         PythonLibraryHelper p = new PythonLibraryHelper();
