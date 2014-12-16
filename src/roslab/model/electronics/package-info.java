986649b8be6139e264f23204818422d80f5529e4
Peter Gebhard <pgeb@seas.upenn.edu>
2014-12-16 16:30:36 -0500

    Deleted package-info classes.

--- src/roslab/model/electronics/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author shaz
- *
- */
-package roslab.model.electronics;
--- src/roslab/model/ui/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides the ROSLab UI model classes.
- * 
- * @author Peter Gebhard
- */
-package roslab.model.ui;
